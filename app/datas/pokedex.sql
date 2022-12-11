61923cb9f26d420047994599fa013d7cf83f78e5
CatherineKeller <catherine.keller.oclock@gmail.com>
2022-12-11 17:10:22 +0100

    fix: ajout SQL for table session

--- app/datas/pokedex.sql
+++ app/datas/pokedex.sql
@@ -449,3 +449,16 @@ INSERT INTO "pokemon_type" ("pokemon_numero", "type_id") VALUES
 (151, 12);
 
 -- --------------------------------------------------------
+
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+
+-- --------------------------------------------------------
